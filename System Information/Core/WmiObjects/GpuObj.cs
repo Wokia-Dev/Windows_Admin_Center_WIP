7272cd5dd7756158199db9aab19f40beae5cb046
Quentin <quentin.ruhf@gmail.com>
2022-09-08 22:21:17 +0200

    Get Gpu info

--- System Information/Core/WmiObjects/GpuObj.cs
+++ System Information/Core/WmiObjects/GpuObj.cs
@@ -2,15 +2,50 @@
 
 public class GpuObj
 {
-    public string Caption { get; set; }
-    public string CompanyName { get; set; }
-    public string Name { get; set; }
+    public string? Caption { get; set; }
+    public string? CompanyName { get; set; }
+    public string? Name { get; set; }
+    public string? VideoProcessor { get; set; }
+    public string? VideoModeDescription { get; set; }
+    public string? MemorySize { get; set; }
+    public string? VideoArchitecture { get; set; }
+    public string? VideoMemoryType { get; set; }
+    public string? CurrentResolution { get; set; }
+    public string? BitsPerPixel { get; set; }
+    public string? CurrentNumberOfColors { get; set; }
+    public string? AdapterDacType { get; set; }
+    public string? InfFileName { get; set; }
+    public string? InfSection { get; set; }
+    public string? MinMaxRefreshRate { get; set; }
+    public string? PnpDeviceId { get; set; }
+    public string? DriverDate { get; set; }
+    public string? DriverVersion { get; set; }
+    public string? InstalledDisplayDrivers { get; set; }
 
-    public GpuObj(string caption, string companyName, string name)
+    public GpuObj(string? caption, string? companyName, string? name, string? videoProcessor,
+        string? videoModeDescription, string? memorySize, string? videoArchitecture, string? videoMemoryType,
+        string? currentResolution, string? bitsPerPixel, string? currentNumberOfColors, string? adapterDacType,
+        string? infFileName, string? infSection, string? minMaxRefreshRate, string? pnpDeviceId, string? driverDate,
+        string? driverVersion, string? installedDisplayDrivers)
     {
         Caption = caption;
         CompanyName = companyName;
         Name = name;
+        VideoProcessor = videoProcessor;
+        VideoModeDescription = videoModeDescription;
+        MemorySize = memorySize;
+        VideoArchitecture = videoArchitecture;
+        VideoMemoryType = videoMemoryType;
+        CurrentResolution = currentResolution;
+        BitsPerPixel = bitsPerPixel;
+        CurrentNumberOfColors = currentNumberOfColors;
+        AdapterDacType = adapterDacType;
+        InfFileName = infFileName;
+        InfSection = infSection;
+        MinMaxRefreshRate = minMaxRefreshRate;
+        PnpDeviceId = pnpDeviceId;
+        DriverDate = driverDate;
+        DriverVersion = driverVersion;
+        InstalledDisplayDrivers = installedDisplayDrivers;
     }
-    
 }
